4dd773f84531ef9236209663e6dc4dd8ff48832a
deepakshrma <deepak.m.shrma@gmail.com>
2017-10-04 01:59:55 +0800

    added enviroment support, more flexible api, added utility, user spawn method intead of exec for color console.out, added color logger

--- test/batman-config.js
+++ test/batman-config.js
@@ -1,13 +1,28 @@
 module.exports = {
-	"ls": {
-		"ls:list": {
+	"ng": {
+		"e2e:cucumber": {
+			"description": "list all files",
 			"env": ['TEST=ENV'],
 			"params": ['e2e'],
 			"options": {
-				"serve": false,
-				"config": "./e2e/config/protractor.cucumber.conf.js",
+				"--serve": false,
+				"--config": "./e2e/config/protractor.cucumber.conf.js",
 				"-wu": "false"
 			}
+		},
+		"new": {
+			"description": "Create new app",
+			"params": ['new', 'app1'],
+			"options": {
+				"--inline-style": true
+			}
+		}
+		
+	},
+	"npm": {
+		"install": {
+			"description": "install files",
+			"params": ['install']
 		}
 	}
 };
