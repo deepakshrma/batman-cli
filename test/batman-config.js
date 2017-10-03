e9d609e8c81d12ca037463d7ac6eca3a1d367e65
Deepak Vishwakarma <deepak.m.shrma@gmail.com>
2017-10-03 18:00:12 +0800

    move code to lib folder

--- test/batman-config.js
+++ test/batman-config.js
@@ -1,16 +1,13 @@
 module.exports = {
-	"ng": {
-		"e2e": [
-			"e2e",
-			{"serve": "false", "-wu": "false"}
-		],
-		"e2e:cucumber": [
-			"e2e",
-			{
-				"serve": "false",
+	"ls": {
+		"ls:list": {
+			"env": ['TEST=ENV'],
+			"params": ['e2e'],
+			"options": {
+				"serve": false,
 				"config": "./e2e/config/protractor.cucumber.conf.js",
 				"-wu": "false"
 			}
-		]
+		}
 	}
 };
