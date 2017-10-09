2eb3275ae022a39983cd4bbe024b908e7cb5230d
deepakshrma <deepak.m.shrma@gmail.com>
2017-10-09 20:36:51 +0800

    move examples to examples folder, lib can be use as require module

--- index.js
+++ index.js
@@ -3,4 +3,16 @@
 
 // Provide a title to the process in `ps`
 process.title = 'batman-cli';
-require("./lib")
+const argv = [...process.argv.slice(2)];
+const {
+    exec
+} = require("./lib");
+const {
+	logger
+} = require('./lib/utils');
+exec(argv[0], (error, data) => {
+    if (error) {
+        logger.error(error);
+    }
+    return process.exit(0);
+});
