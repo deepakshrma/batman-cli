ad4c77cdbf5f23f849324487c3e0f1a1ce4dee79
deepakshrma <deepak.m.shrma@gmail.com>
2017-10-15 23:06:07 +0800

    added commander and refactor code

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 #!/usr/bin/env node
 'use strict';
-
+const program = require('commander');
+const packageJson = require('./package.json');
+// console.log(packageJson);
 // Provide a title to the process in `ps`
 process.title = 'batman-cli';
-const argv = [...process.argv.slice(2)];
 const {
-    exec
+    exec,
+    initConfig,
+    list
 } = require("./lib");
+
 const {
-	logger
+    colors
 } = require('./lib/utils');
-exec(argv[0], (error, data) => {
-    if (error) {
-        logger.error(error);
-    }
-    return process.exit(0);
-});
+
+program
+    .version(packageJson.version);
+program
+    .command('init')
+    .alias('i')
+    .description('initialize batman configuration')
+    .option("-c, --config <path>", "set config path for batman cli. default to ./batman.config.js || ./.batmanrc.json")
+    .option("-js, --js", "defualt is json")
+    .action(initConfig);
+program
+    .command('exec <cmd>')
+    .alias('run')
+    .description('execute the given command cmd')
+    .action(exec);
+program
+    .command('list')
+    .alias('ls')
+    .description('list commands')
+    .option("-j, --json", "defualt is pretty")
+    .action(list);
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp(colors.red);
+}
